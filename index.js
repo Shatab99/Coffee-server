a0d8eef09d312dbd725544cc6b163d4a7d556da6
shatab <shatabag4749@gmail.com>
2024-10-16 20:19:59 +0600

    Making user route

--- index.js
+++ index.js
@@ -89,6 +89,14 @@ async function run() {
             const result = await cursor.toArray(cursor);
             res.send(result)
         })
+        
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email }
+            const result = await userCollection.findOne(query)
+            res.send(result)
+        })
+
 
 
         app.post('/users', async(req,res)=>{
